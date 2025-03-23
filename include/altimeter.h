e934fa5acfe025bd682d4618361934ee1ae1e571
httpsdluc <dlucero0715@gmail.com>
2025-03-23 19:38:26 -0400

    03/25 latest update

--- include/altimeter.h
+++ include/altimeter.h
@@ -4,6 +4,12 @@
 #include <SPI.h>
 #include <Adafruit_BMP3XX.h>
 
+// SPI1 Pin Definitions (adjust if needed)
+#define BMP_CS     10  // Chip Select pin for BMP388
+#define SPI1_SCK   27  // SPI1 Clock
+#define SPI1_MISO  39  // SPI1 MISO
+#define SPI1_MOSI  28  // SPI1 MOSI
+
 // Define the altimeter object
 extern Adafruit_BMP3XX altimeter;
 
